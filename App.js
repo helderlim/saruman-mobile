fa14b9eaaf47a11e41bb551e91a58efba32446b9
helderlim <helderfelima16@gmail.com>
2022-04-07 01:05:36 -0300

    consulta de imagens teste passar para components

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect ,useState } from 'react';
+import { StyleSheet, Text, View, Image, SafeAreaView, ScrollView } from 'react-native';
+import api from './services/api'
 
 export default function App() {
+  const [photos, setPhotos] = useState([]);
+  console.log('photos', photos)
+
+   useEffect(() => {
+    async function getPost() {
+    const response = await api.get()
+    setPhotos(response.data)
+    }
+    getPost();
+  }, [])
   return (
+    <SafeAreaView style={styles.container}>
+    <ScrollView style={styles.scrollView}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {photos.map((pic) =>
+      <View key={pic.id}>
+        <Image
+        style={styles.image}
+        source={{
+          uri: pic.urls.full,
+        }} />
+
+      </View>
+      )
+    }
     </View>
+    </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -16,5 +40,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    width: 600
+  },
+  image: {
+    height: 300,
+    width: 100
+  },
+  scrollView: {
+    backgroundColor: 'pink',
+    marginHorizontal: 20,
   },
 });
