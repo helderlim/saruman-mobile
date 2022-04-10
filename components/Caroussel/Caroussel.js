a21012dbb49962f3719c5580ddc17fc3021ee9ea
helderlim <helderfelima16@gmail.com>
2022-04-09 21:35:57 -0300

    feat: adiciona caroussel e pernso components

---
+++ components/Caroussel/Caroussel.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { StatusBar, FlatList, Image, Animated, Text, View, Dimensions, StyleSheet, TouchableOpacity } from 'react-native';
+const { width, height } = Dimensions.get('screen');
+
+const data = [];
+
+const imageW = width * 1.00;
+const imageH = imageW * 0.6;
+
+export default (props) => {
+  const listPhotos = props.listPhotos.map((urls) => urls.urls)
+  return (
+    <View style={{
+      flex: 1, backgroundColor: '#000', width: imageW,
+      height: imageH,
+    }}>
+      <FlatList
+        data={listPhotos}
+        keyExtractor={(_, index) => index.toString()}
+        horizontal
+        pagingEnabled
+        renderItem={({ item }) => {
+          return <View style={{ width, justifyContent: 'center' }}>
+            <Image source={{ uri: item.regular }} style={{
+              width: imageW,
+              height: imageH,
+              resizeMode: 'cover',
+              borderRadius: 16
+            }} />
+          </View>
+        }}
+      />
+    </View>
+  );
+};
--- pages/feed/Feed.js
+++ pages/feed/Feed.js
@@ -1,6 +1,8 @@
-import React, { useEffect ,useState } from 'react';
-import { StyleSheet, Text, View, Image, SafeAreaView, ScrollView, Button, ActivityIndicator, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Image, SafeAreaView, ScrollView, ActivityIndicator, TouchableOpacity } from 'react-native';
 import axios from 'axios';
+import Person from './Person';
+import Caroussel from '../../components/Caroussel/Caroussel';
 
 export default function Feed({ navigation }) {
   const [photos, setPhotos] = useState([]);
@@ -8,38 +10,51 @@ export default function Feed({ navigation }) {
 
   useEffect(() => {
     async function getPost() {
-    const response = await axios
-    .get(
-      `https://api.unsplash.com/search/photos/?page=1&per_page=20&query=hotels&client_id=EnxW4t-KHx4sO5n8JwvVvPmQ_WJKE5DN8X6lj0oW2Mo`
-    )
-    setPhotos(response.data.results)
+      const response = await axios
+        .get(
+          `https://api.unsplash.com/search/photos/?page=1&per_page=20&query=hotels&client_id=EnxW4t-KHx4sO5n8JwvVvPmQ_WJKE5DN8X6lj0oW2Mo`
+        )
+      setPhotos(response.data.results)
     }
     getPost();
   }, [])
+
+  const listPhotos = photos.map((urls) => urls.urls)
+  var novoArray = []
+  const corte = 3
+  for (var i = 0; i < 3; i = i + corte) {
+    novoArray.push(listPhotos.slice(i, i + corte));
+  }
+  console.log('listPhotos', listPhotos)
+  console.log('novoArray', novoArray[0])
+
   return (
     <SafeAreaView style={styles.container}>
-    <ScrollView>
-    <View>
-      {photos.length ? photos.map((pic) =>
-      <View key={pic.id}>
-        <TouchableOpacity
-        onPress={() => navigation.navigate('Details', {
-          ProfileName: pic.user.username
-        })}
-        >
-          <Image
-          style={styles.image}
-          source={{
-            uri: pic.urls.regular,
-          }} />
-        </TouchableOpacity>
-        <Text>{pic.user.name}</Text>
-      </View>
-      )
-      : <ActivityIndicator size="large" color="#0000ff" />
-    }
-    </View>
-    </ScrollView>
+      <ScrollView>
+        <View style={styles.cardList}>
+          <Text style={styles.title}>Destaques da</Text>
+          <Text style={styles.title}>Semana</Text>
+          <Text style={styles.subtitle}>QUER VIAJAR HOJE?</Text>
+          {photos.length
+            ? photos.map((pic) =>
+              <View key={pic.id} style={styles.card}>
+                <TouchableOpacity
+                  onPress={() => navigation.navigate('Details', {
+                    ProfileName: pic.user.username
+                  })}
+                >
+                  <Image
+                    style={styles.image}
+                    source={{
+                      uri: pic.urls.regular,
+                    }} />
+                </TouchableOpacity>
+                <Person profile={pic.user} />
+              </View>
+            ) : <ActivityIndicator size="large" color="#0000ff" />
+          }
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -47,12 +62,27 @@ export default function Feed({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#121212',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: 'white',
+    fontSize: 36
+  },
+  subtitle: {
+    fontSize: 13,
+    color: 'white'
+  },
   image: {
-    height: 600 ,
-    width: 400
+    height: 343,
+    width: 343,
+    borderRadius: 16
+  },
+  cardList: {
+  },
+  card: {
+    marginBottom: 12,
+    display: 'flex'
   }
 });
